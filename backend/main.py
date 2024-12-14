a66a9c8df65aba6f8d12093fd56df9a1d6958bba
Icelycoool <mohammedcali@proton.me>
2024-12-14 23:35:29 +0300

    Initilized the project and successfully run a simple get endpoint

---
+++ backend/main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_restx import Api, Resource
+from exts import db
+
+
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    db.init_app(app)
+
+    api = Api(app,
+              title='Bookaspot',
+              version='1.0',
+              description='Bookaspot is an online platform where users can easily rent and lease various amenities such as swimming pools, event halls, and football stadiums.')
+
+    @api.route('/hello')
+    class Hello(Resource):
+        def get(self):
+            return {'message': 'Hello World!'}
+
+
+    return app
